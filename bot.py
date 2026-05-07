65ab293b149a6dafa61ec07a8ab391739adba5a2
diseVse <bulgakov.alexandr.08@yandex.ru>
2026-05-07 12:59:17 +0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,25 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+
+from config import BOT_TOKEN
+from handlers import router
+from database import init_db
+
+
+async def main():
+    bot = Bot(token=BOT_TOKEN)
+
+    dp = Dispatcher()
+
+    dp.include_router(router)
+
+    await init_db()
+
+    print("Бот запущен")
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
